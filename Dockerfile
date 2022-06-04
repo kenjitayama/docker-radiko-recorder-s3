605635bf724f493864171d9fe401cc9c91fea73b
Kenji Tayama <kenji.tayama@gmail.com>
2022-06-04 18:58:53 +0900

    program name in filename

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM atsnngs/radish:latest
 MAINTAINER Atsushi Nagase<a@ngs.io>
 
-RUN apt-get update -y && apt-get install -y python-pip curl
+RUN apt-get update -y && apt-get install -y python-pip curl nkf
 RUN pip install awscli
 ADD rec_radiko3.sh /usr/local/bin/rec_radiko3.sh
 
--- rec_radiko3.sh
+++ rec_radiko3.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
+function urlencode {
+  echo "$1" | nkf -WwMQ | sed 's/=$//g' | tr = % | tr -d '\n'
+}
+
 set -eu
 
 USERNAME='Radiko Recorder'
 DIR=/var/radiko
 TIME=$(date +"%Y-%m-%d-%H_%M")
+URLENCODED_PROGRAM_NAME=`urlencode "${PROGRAM_NAME}"`
 FNAME="${PROGRAM_NAME}_${STATION}_${TIME}.m4a"
-OUTFILE="${DIR}/${FNAME}"
+URLENCODED_FNAME="${URLENCODED_PROGRAM_NAME}_${STATION}_${TIME}.m4a"
+OUTFILE="${DIR}/${URLENCODED_FNAME}"
 mkdir -p $DIR
 
 if [ -n "$SLACK_WEBHOOK_URL" ]; then
@@ -29,12 +35,12 @@ else
     -o $OUTFILE
 fi
 
-echo "Uploading $FNAME"
-aws s3 cp --acl public-read "$OUTFILE" "s3://${S3_BUCKET}/${FNAME}"
-URL="https://${S3_BUCKET}.s3.amazonaws.com/${FNAME}"
+echo "Uploading \"$FNAME\""
+aws s3 cp --acl public-read "$OUTFILE" "s3://${S3_BUCKET}/${URLENCODED_FNAME}"
+URL="https://${S3_BUCKET}.s3.amazonaws.com/${URLENCODED_FNAME}"
 
 
 if [ -n "$SLACK_WEBHOOK_URL" ]; then
-  curl -s -X POST --data-urlencode "payload={\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"${USERNAME}\", \"text\": \"Completed recording ${STATION} for ${DURATION_MINUTES} minutes\",\"attachments\":[{\"title\":\"${FNAME}\",\"title_link\":\"${URL}\"}]}" $SLACK_WEBHOOK_URL
+  curl -s -X POST --data-urlencode "payload={\"channel\": \"#${SLACK_CHANNEL}\", \"username\": \"${USERNAME}\", \"text\": \"Completed recording ${STATION} for ${DURATION_MINUTES} minutes\",\"attachments\":[{\"title\":\"${URLENCODED_FNAME}\",\"title_link\":\"${URL}\"}]}" $SLACK_WEBHOOK_URL
 fi
 
